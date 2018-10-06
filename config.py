d12a1f0f95b3a2497b1b20e54de01ff65ff10b24
David Jones <davejonesbkk@gmail.com>
2018-10-06 08:13:45 +0700

    Config file for storing Reddit & Twitter API info

---
+++ config.py
@@ -0,0 +1,17 @@
+twitter_credentials = dict(
+
+	ACCESS_TOKEN = '**********',
+	ACCESS_SECRET = '************',
+	CONSUMER_KEY = '**********',
+	CONSUMER_SECRET = '********',
+	gmail_user = 'Your email',
+	gmail_passwd = '****'
+)
+
+reddit_credentials = dict(
+
+	client_id='****',
+	client_secret='*****',
+	user_agent='Coinreddits by /u/your_reddit_name
+
+)
